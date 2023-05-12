b03789291826ef5b9d11f6d2e994f8e4c2207982
Alan Vieira Marins <alanhorcrux@gmail.com>
2023-05-12 13:30:01 -0300

    rename-identity

--- ApiAuth/Migrations/20230512132125_CustomIdentity.Designer.cs
+++ ApiAuth/Migrations/20230512132125_CustomIdentity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ApiAuth.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ApiAuth.Migrations
 {
     [DbContext(typeof(UsuarioDbContext))]
-    partial class UsuarioDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230512132125_CustomIdentity")]
+    partial class CustomIdentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
